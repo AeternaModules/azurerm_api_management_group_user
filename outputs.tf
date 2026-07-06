bb279e6f3a2e71162119fd1d02f90b0f89a2e362
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:10:51 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,20 @@ output "api_management_group_users" {
   description = "All api_management_group_user resources"
   value       = azurerm_api_management_group_user.api_management_group_users
 }
+output "api_management_group_users_api_management_name" {
+  description = "List of api_management_name values across all api_management_group_users"
+  value       = [for k, v in azurerm_api_management_group_user.api_management_group_users : v.api_management_name]
+}
+output "api_management_group_users_group_name" {
+  description = "List of group_name values across all api_management_group_users"
+  value       = [for k, v in azurerm_api_management_group_user.api_management_group_users : v.group_name]
+}
+output "api_management_group_users_resource_group_name" {
+  description = "List of resource_group_name values across all api_management_group_users"
+  value       = [for k, v in azurerm_api_management_group_user.api_management_group_users : v.resource_group_name]
+}
+output "api_management_group_users_user_id" {
+  description = "List of user_id values across all api_management_group_users"
+  value       = [for k, v in azurerm_api_management_group_user.api_management_group_users : v.user_id]
+}
 
