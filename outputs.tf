48533d922c56fc11e5a4c4b6b6f3275203da9208
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "api_management_group_users" {
-  description = "All api_management_group_user resources"
-  value       = azurerm_api_management_group_user.api_management_group_users
-}
 output "api_management_group_users_api_management_name" {
-  description = "List of api_management_name values across all api_management_group_users"
-  value       = [for k, v in azurerm_api_management_group_user.api_management_group_users : v.api_management_name]
+  description = "Map of api_management_name values across all api_management_group_users, keyed the same as var.api_management_group_users"
+  value       = { for k, v in azurerm_api_management_group_user.api_management_group_users : k => v.api_management_name }
 }
 output "api_management_group_users_group_name" {
-  description = "List of group_name values across all api_management_group_users"
-  value       = [for k, v in azurerm_api_management_group_user.api_management_group_users : v.group_name]
+  description = "Map of group_name values across all api_management_group_users, keyed the same as var.api_management_group_users"
+  value       = { for k, v in azurerm_api_management_group_user.api_management_group_users : k => v.group_name }
 }
 output "api_management_group_users_resource_group_name" {
-  description = "List of resource_group_name values across all api_management_group_users"
-  value       = [for k, v in azurerm_api_management_group_user.api_management_group_users : v.resource_group_name]
+  description = "Map of resource_group_name values across all api_management_group_users, keyed the same as var.api_management_group_users"
+  value       = { for k, v in azurerm_api_management_group_user.api_management_group_users : k => v.resource_group_name }
 }
 output "api_management_group_users_user_id" {
-  description = "List of user_id values across all api_management_group_users"
-  value       = [for k, v in azurerm_api_management_group_user.api_management_group_users : v.user_id]
+  description = "Map of user_id values across all api_management_group_users, keyed the same as var.api_management_group_users"
+  value       = { for k, v in azurerm_api_management_group_user.api_management_group_users : k => v.user_id }
 }
 
